686811fbd1329fce8a153d2965c57e7b9d6d6b59
jckdm <jack.adam@yale.edu>
2021-04-06 00:26:59 -0400

    25 days later, i realize my javascript SUCKED. took some time to give it love.

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ let rightselectedButton = null;
 
 let selectedScheme;
 
-const classic = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#EE82EE'];
-const vintage = ['#91A16A', '#B36154', '#738986', '#DDC173', '#8E8680', '#B29576', '#C3BDCE'];
-const grayscale = ['#EEEEEE', '#CCCCCC', '#999999', '#777777', '#555555', '#333333', '#111111'];
-const pastel = ['#F0CAD6', '#F8E0DE', '#FCF5DB', '#E8F1DE', '#D9E4FB', '#E0D3E3', '#FFF0F3'];
-const protanopia = ['#001159', '#2F437F', '#576696', '#848895', '#AEA17A', '#EAC243', '#FFD429'];
-const deuteranopia = ['#081951', '#2C3E7A', '#3E5294', '#636F97', '#878581', '#DFBE50', '#FFE138'];
-const tritanopia = ['#274750', '#007E7A', '#00A698', '#00C3A9', '#9EC8AC', '#FFC6AD', '#4E061A'];
+const SCHEMES = {
+       'classic': ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#EE82EE'],
+       'vintage': ['#91A16A', '#B36154', '#738986', '#DDC173', '#8E8680', '#B29576', '#C3BDCE'],
+     'grayscale': ['#EEEEEE', '#CCCCCC', '#999999', '#777777', '#555555', '#333333', '#111111'],
+        'pastel': ['#F0CAD6', '#F8E0DE', '#FCF5DB', '#E8F1DE', '#D9E4FB', '#E0D3E3', '#FFF0F3'],
+    'protanopia': ['#001159', '#2F437F', '#576696', '#848895', '#AEA17A', '#EAC243', '#FFD429'],
+  'deuteranopia': ['#081951', '#2C3E7A', '#3E5294', '#636F97', '#878581', '#DFBE50', '#FFE138'],
+    'tritanopia': ['#274750', '#007E7A', '#00A698', '#00C3A9', '#9EC8AC', '#FFC6AD', '#4E061A']
+}
 
 let text;
 let starting = true;
@@ -29,14 +31,15 @@ setup = () => {
 
 go = () => {
   if (!starting) {
-    if (text.join('\n') == $('#inp')[0].value) {
+    // technically this fails if you go to edit the placeholder text and don't actually edit it, but why would you do that
+    if (text.join('\n') === $('#inp')[0].value) {
       cancel();
       return;
     }
   }
   const val = $('#inp')[0].value.split('\n');
-  text = (val == '') ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a magna ante. Morbi ut ligula enim. Phasellus vulputate dapibus molestie. Integer tristique nisl sed libero elementum, eget pretium velit varius. Donec rhoncus tellus sed eros laoreet semper. Curabitur ultrices turpis id tortor mollis hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque sodales nunc eget elit iaculis tempor.'.split('\n') : val;
-  if (text != '') {
+  text = (val[0] === "" && val.length === 1) ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a magna ante. Morbi ut ligula enim. Phasellus vulputate dapibus molestie. Integer tristique nisl sed libero elementum, eget pretium velit varius. Donec rhoncus tellus sed eros laoreet semper. Curabitur ultrices turpis id tortor mollis hendrerit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque sodales nunc eget elit iaculis tempor.'.split('\n') : val;
+  if (text !== '') {
     cancel();
     if (starting) { $('.overlay3').css('visibility', 'visible'); }
     else { id(text); }
@@ -70,32 +73,32 @@ edit = (x) => {
 randomly = () => {
   const alltext = $('p text');
   const swatches = $('#left-menu circle');
-  const shapes = $('#shapes')[0].children;
+  const shapez = $('#shapes')[0].children;
   $('.box').remove();
 
   const rs = $('#rs')[0].value;
   const rc = $('#rc')[0].value;
 
-  if (rs == 'all' && rc == 'all') {
+  if (rs === 'all' && rc === 'all') {
     for (letter of alltext) {
       leftcolor(swatches[Math.floor(Math.random() * 7)]);
-      rightshape(shapes[Math.floor(Math.random() * 7)]);
+      rightshape(shapez[Math.floor(Math.random() * 7)]);
       letter.click();
       letter.style.color = c;
     }
   }
-  else if (rs == 'all') {
+  else if (rs === 'all') {
     for (letter of alltext) {
       leftcolor(swatches[+rc]);
-      rightshape(shapes[Math.floor(Math.random() * 7)]);
+      rightshape(shapez[Math.floor(Math.random() * 7)]);
       letter.click();
       letter.style.color = c;
     }
   }
-  else if (rc == 'all') {
+  else if (rc === 'all') {
     for (letter of alltext) {
       leftcolor(swatches[Math.floor(Math.random() * 7)]);
-      rightshape(shapes[+rs]);
+      rightshape(shapez[+rs]);
       letter.click();
       letter.style.color = c;
     }
@@ -103,16 +106,16 @@ randomly = () => {
   else {
     for (letter of alltext) {
       leftcolor(swatches[+rc]);
-      rightshape(shapes[+rs]);
+      rightshape(shapez[+rs]);
       letter.click();
       letter.style.color = c;
     }
   }
-  $('#unhide')[0].value = (alltext.css('color') == 'rgb(38, 38, 38)') ? 'Hide letters' : 'Show hidden letters';
+  $('#unhide')[0].value = (alltext.css('color') === 'rgb(38, 38, 38)') ? 'Hide letters' : 'Show hidden letters';
 }
 
 color = (sc) => {
-  selectedScheme = eval(sc);
+  selectedScheme = SCHEMES[sc];
   const swatches = $('#scheme circle');
   const randcolmen = $('#rc')[0].children;
   for (let i = 0; i < 7; i++) {
@@ -131,11 +134,11 @@ color = (sc) => {
 
 settings = () => {
   $('.overlay4').css('visibility', 'visible');
-  $('#unhide')[0].value = ($('p text').css('color') == 'rgb(38, 38, 38)') ? 'Hide letters' : 'Show hidden letters';
+  $('#unhide')[0].value = ($('p text').css('color') === 'rgb(38, 38, 38)') ? 'Hide letters' : 'Show hidden letters';
 }
 
 unhide = () => {
-  if ($('p text').css('color') == 'rgb(38, 38, 38)') {
+  if ($('p text').css('color') === 'rgb(38, 38, 38)') {
     $('p text').css('color', '#FFFAF0');
     $('#unhide')[0].value = 'Show hidden letters';
   }
@@ -157,26 +160,42 @@ id = (text) => {
   let j = 0;
 
   for (let i = 0; i < text.length; i++) {
-    $('body').append('<p id="_p' + i + '"></p>');
+    const pp = document.createElement('p');
+    pp.setAttribute('id', `_p${i}`);
+    document.body.appendChild(pp);
     const sentence = text[i].split(' ');
+
     for (word of sentence) {
       w = word.split('');
       for (c of w) {
-        $('#_p' + i).append('<text id="_' + j + '">' + c + '</text>');
+        const pt = document.createElement('text');
+        pt.setAttribute('id', `_${j}`)
+        pt.innerText = c;
+        pp.appendChild(pt);
         j++;
       }
-      $('#_p' + i).append(' ')
+      pp.append(' ')
     }
   }
 
   $('text').click(function() {
     if (leftselected && rightselected) {
 
-      $('body').prepend('<div style="top:' + this.offsetTop + 'px; left:' + this.offsetLeft + 'px;" id="' + this.id + '" class="box" onclick="letvis(this.id)"><svg id="cement"></svg></div>');
+      const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      svg.setAttribute('id', 'cement');
+      svg.setAttribute('class', this.id);
+      svg.setAttribute('width', this.offsetWidth);
+      svg.setAttribute('height', this.offsetHeight);
 
-      $('#cement').attr('class', this.id);
-      $('#cement').attr('width', this.offsetWidth);
-      $('#cement').attr('height', this.offsetHeight);
+      const div = document.createElement('div');
+      div.style.top = this.offsetTop + 'px';
+      div.style.left = this.offsetLeft + 'px';
+      div.setAttribute('id', this.id);
+      div.setAttribute('class', 'box');
+      div.setAttribute('onclick', 'letvis(this.id)');
+
+      div.append(svg);
+      $('body').prepend(div);
 
       const h = this.offsetHeight;
       const w = this.offsetWidth;
@@ -185,7 +204,7 @@ id = (text) => {
 
       if ($('#hidetext')[0].checked) { this.style.color = '#fffaf0'; }
 
-      if (rightselectedShape == 'rect') {
+      if (rightselectedShape === 'rect') {
         const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
 
         rect.setAttribute('stroke', '#262626');
@@ -194,10 +213,11 @@ id = (text) => {
         rect.setAttribute('width', w);
         rect.setAttribute('fill', leftselectedColor);
         rect.setAttribute('class', this.id);
+        rect.setAttribute('name', rightselectedShape);
 
-        $('#cement').append(rect);
+        svg.append(rect);
       }
-      else if (rightselectedShape == 'ellipse') {
+      else if (rightselectedShape === 'ellipse') {
         const ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'ellipse');
 
         ellipse.setAttribute('stroke', '#262626');
@@ -208,33 +228,55 @@ id = (text) => {
         ellipse.setAttribute('ry', midH);
         ellipse.setAttribute('fill', leftselectedColor);
         ellipse.setAttribute('class', this.id);
+        ellipse.setAttribute('name', rightselectedShape);
 
-        $('#cement').append(ellipse);
+        svg.append(ellipse);
       }
-      else if (rightselectedShape == 'pentagon') {
-        $('#cement').html('<polygon name="pentagon" class="' + this.id + '" points="' + pentagon(h, w, midW, midH) + '" stroke="#262626" stroke-width="0.5" fill="' + leftselectedColor +'"/>');
-      }
-      else if (rightselectedShape == 'hexagon') {
-        $('#cement').html('<polygon name="hexagon" class="' + this.id + '" points="' + hexagon(h, w, midW, midH) + '" stroke="#262626" stroke-width="0.5" fill="' + leftselectedColor +'"/>');
-      }
-      else if (rightselectedShape == 'triangle') {
-        $('#cement').html('<polygon name="triangle" class="' + this.id + '" points="' + triangle(h, w, midW) + '" stroke="#262626" stroke-width="0.5" fill="' + leftselectedColor +'"/>');
-      }
-      else if (rightselectedShape == 'chevron') {
-        $('#cement').html('<polygon name="chevron" class="' + this.id + '" points="' + chevron(h, w, midW) + '" stroke="#262626" stroke-width="0.5" fill="' + leftselectedColor +'"/>');
-      }
-      else if (rightselectedShape == 'octagon') {
-        $('#cement').html('<polygon name="octagon" class="' + this.id + '" points="' + octagon(h, w) + '" stroke="#262626" stroke-width="0.5" fill="' + leftselectedColor +'"/>');
+      else {
+        const poly = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+
+        poly.setAttribute('class', this.id);
+        poly.setAttribute('points', SHAPES[rightselectedShape](h, w, midW));
+        poly.setAttribute('stroke', '#262626');
+        poly.setAttribute('stroke-width', 0.5);
+        poly.setAttribute('fill', leftselectedColor);
+        poly.setAttribute('name', rightselectedShape);
+
+        svg.append(poly);
       }
     }
   })
 }
 
+const SHAPES = {
+  'pentagon': pentagon = (h, w, midW) => {
+    const qW = w * 0.266;
+    const pH = h * 0.366;
+    return `${midW} 0, ${w} ${pH}, ${midW + qW} ${h}, ${midW - qW} ${h}, 0 ${pH}`
+  },
+  'hexagon': hexagon = (h, w, midW) => {
+    const hp = h * 0.266;
+    return `${midW} 0, ${w} ${hp}, ${w} ${h - hp}, ${midW} ${h}, 0 ${h - hp}, 0 ${hp}`
+  },
+  'chevron': chevron = (h, w, midW) => {
+    const pH = h * 0.65;
+    return `${midW} 0, ${w} ${h}, ${midW} ${pH}, 0 ${h}`
+  },
+  'octagon': octagon = (h, w) => {
+    const w3 = w / 3.0;
+    const w23 = w * (2.0 / 3.0);
+    const h3 = h / 3.0;
+    const h23 = h * (2.0 / 3.0);
+    return `${w3} 0, ${w23} 0, ${w} ${h3}, ${w} ${h23}, ${w23} ${h}, ${w3} ${h}, 0 ${h23}, 0 ${h3}`
+  },
+  'triangle': triangle = (h, w, midW) => `${midW} 0, ${w} ${h}, 0 ${h}`
+}
+
 letvis = (i) => {
-  if ($('#hidetext')[0].checked || (!$('#hidetext')[0].checked && $('text#' + i)[0].style.color == 'rgb(255, 250, 240)')) {
-    $('text#' + i)[0].style.color = '#262626';
+  if ($('#hidetext')[0].checked || (!$('#hidetext')[0].checked && $('text#' + i)[0].style.color === 'rgb(255, 250, 240)')) {
+    $(`text#${i}`)[0].style.color = '#262626';
   }
-  $('div#' + i)[0].remove();
+  $(`div#${i}`)[0].remove();
   ;
 }
 
@@ -261,79 +303,36 @@ $(window).resize(() => {
 
   const boxes = $('div.box');
   for (box of boxes) {
-    const letter = $('text#' + box.attributes.id.value)[0];
+    const id = box.attributes.id.value;
+    const letter = $(`text#${id}`)[0];
 
     $(box).css('top', letter.offsetTop);
     $(box).css('left', letter.offsetLeft);
     $(box).css('height', letter.offsetHeight);
     $(box).css('width', letter.offsetWidth);
 
-    $('#cement' + '.' + box.attributes.id.value)[0].setAttribute('width', letter.offsetWidth);
-    $('#cement' + '.' + box.attributes.id.value)[0].setAttribute('height', letter.offsetHeight);
+    $(`#cement.${id}`)[0].setAttribute('width', letter.offsetWidth);
+    $(`#cement.${id}`)[0].setAttribute('height', letter.offsetHeight);
 
-    polyresize($('#cement' + '.' + box.attributes.id.value)[0].firstChild.tagName, box.attributes.id.value, letter.offsetWidth, letter.offsetHeight);
+    polyresize($(`#cement.${id}`)[0].firstChild.attributes.name.value, box.attributes.id.value, letter.offsetWidth, letter.offsetHeight);
   }
 })
 
 polyresize = (tag, cla, w, h) => {
   const midW = w / 2.0;
-  const midH = h / 2.0;
 
-  if (tag == 'rect') {
-    $('rect' + '.' + cla)[0].setAttribute('width', w);
-    $('rect' + '.' + cla)[0].setAttribute('height', h);
+  if (tag === 'rect') {
+    $(`rect.${cla}`)[0].setAttribute('width', w);
+    $(`rect.${cla}`)[0].setAttribute('height', h);
   }
-  else if (tag == 'ellipse') {
-    $('ellipse' + '.' + cla)[0].setAttribute('cy', midH);
-    $('ellipse' + '.' + cla)[0].setAttribute('cx', midW);
-    $('ellipse' + '.' + cla)[0].setAttribute('rx', midW);
-    $('ellipse' + '.' + cla)[0].setAttribute('ry', midH);
+  else if (tag === 'ellipse') {
+    const midH = h / 2.0;
+    $(`ellipse.${cla}`)[0].setAttribute('cy', midH);
+    $(`ellipse.${cla}`)[0].setAttribute('cx', midW);
+    $(`ellipse.${cla}`)[0].setAttribute('rx', midW);
+    $(`ellipse.${cla}`)[0].setAttribute('ry', midH);
   }
-  else if (tag == 'polygon') {
-    const name = $('polygon' + '.' + cla)[0].attributes.name.value;
-    const points = $('polygon' + '.' + cla)[0].attributes.points.value;
-    if (name == 'pentagon') {
-      const qW = w * 0.3195;
-      $('polygon' + '.' + cla)[0].attributes.points.value = pentagon(h, w, midW, midH);
-    }
-    else if (name == 'hexagon') {
-      const hp = h * 0.266;
-      $('polygon' + '.' + cla)[0].attributes.points.value = hexagon(h, w, midW, midH);
-    }
-    else if (name == 'triangle') {
-      $('polygon' + '.' + cla)[0].attributes.points.value = triangle(h, w, midW);
-    }
-    else if (name == 'chevron') {
-      $('polygon' + '.' + cla)[0].attributes.points.value = chevron(h, w, midW);
-    }
-    else if (name == 'octagon') {
-      $('polygon' + '.' + cla)[0].attributes.points.value = octagon(h, w);
-    }
+  else {
+    $(`polygon.${cla}`)[0].attributes.points.value = SHAPES[tag](h, w, midW);
   }
 }
-
-pentagon = (h, w, midW, midH) => {
-  const qW = w * 0.266;
-  const pH = h * 0.366;
-  return String(midW) + ' 0,' + String(w) + ' ' + String(pH) + ',' + String(midW + qW) + ' ' + String(h) + ',' + String(midW - qW) + ' ' + String(h) + ',' + '0 ' + String(pH);
-}
-
-hexagon = (h, w, midW, midH) => {
-  const hp = h * 0.266;
-  return String(midW) + ' 0,' + String(w) + ' ' + String(hp) + ',' + String(w) + ' ' + String(h - hp) + ',' + String(midW) + ' ' + String(h) + ',' + '0 ' + String(h - hp) + ',' + '0 ' + String(hp);
-}
-
-chevron = (h, w, midW) => {
-  const pH = h * 0.65;
-  return String(midW) + ' 0,' + String(w) + ' ' + String(h) + ',' + String(midW) + ' ' + String(pH) + ',' + '0 ' + String(h);
-}
-
-octagon = (h, w) => {
-  const w3 = w / 3.0;
-  const w23 = w * (2.0 / 3.0);
-  const h3 = h / 3.0;
-  const h23 = h * (2.0 / 3.0);
-  return String(w3) + ' 0,' + String(w23) + ' 0,' + String(w) + ' ' + String(h3) + ',' + String(w) + ' ' + String(h23) + ',' + String(w23) + ' ' + String(h) + ',' + String(w3) + ' ' + String(h) + ',' + '0 ' + String(h23) + ',' + '0 ' + String(h3);
-}
-
-triangle = (h, w, midW) => String(midW) + ' 0,' + String(w) + ' ' + String(h) + ',' + '0 ' + String(h);
